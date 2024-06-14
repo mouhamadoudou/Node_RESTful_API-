57aaac5975727260f0f8182b95c952deb3197ec0
Mouhamad <mouhamad@192.168.1.18>
2024-06-14 18:22:23 +0000

    add :  add MongoDb

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-
+const PORT = process.env.PORT;
 
 const app = express();
 
 
-app.listen(PORT, () => {
-    console.log("server is connected to " + PORT)
-})
+mongoose.connect(dbURI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log("server is connected to " + PORT + " and connected to mangoDb")
+        })
+    })
+    .catch((error) => {
+        console.log('Unable to connect to server and/or MangoDb')
+    })
+
 
 app.use(bodyParser.json())
 app.use(cors())
